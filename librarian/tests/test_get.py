895355cf6b2c7f9dda0ce41365c8bb8d7bd357d3
31grudnia <mikolaj.starczewski@gmail.com>
2025-11-28 03:08:01 +0100

    Get field_value tests

--- librarian/tests/test_get.py
+++ librarian/tests/test_get.py
@@ -3,7 +3,7 @@ import pytest
 from librarian.database.models.file import HeaderModel, FooterModel, TransactionModel
 from librarian.src.crud import get_file_section, get_file_field
 
-
+# Get section tests
 def test_get_file_section_header(seeded_db):
     result = get_file_section(db=seeded_db, file_id=1, section_id=1)
     assert isinstance(result, HeaderModel)
@@ -25,3 +25,27 @@ def test_get_file_section_invalid_id(seeded_db):
     with pytest.raises(ValueError) as excinfo:
         get_file_section(seeded_db, file_id=1, section_id=99)
     assert "Invalid section" in str(excinfo.value)
+
+# Get field_value tests
+def test_get_field_from_header(seeded_db):
+    val = get_file_field(seeded_db, file_id=1, field_value="surname")
+    assert val == "Doe"
+
+def test_get_field_from_footer(seeded_db):
+    val = get_file_field(seeded_db, file_id=1, field_value="total_counter")
+    assert val == 2
+
+def test_get_field_from_transaction(seeded_db):
+    val = get_file_field(seeded_db, file_id=1, field_value="amount")
+    assert isinstance(val, list)
+    assert val == [100, 200]
+
+def test_get_field_non_existent_field(seeded_db):
+    with pytest.raises(ValueError) as excinfo:
+        get_file_field(seeded_db, file_id=1, field_value="potato_salad")
+    assert "does NOT exist" in str(excinfo.value)
+
+def test_get_field_non_existent_file(seeded_db):
+    with pytest.raises(ValueError) as excinfo:
+        get_file_field(seeded_db, file_id=999, field_value="surname")
+    assert "File with id:999 does NOT exist" in str(excinfo.value)
